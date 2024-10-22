c42049a2c43878eb498bf08d5fe6afbf8414cd33
Vicente García Martínez <vis_97@outlook.com>
2024-10-22 14:09:54 -0600

    Update test_SS.py

--- test/test_SS.py
+++ test/test_SS.py
@@ -1,10 +1,28 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture(scope="module")
 def driver():
     # Configuración inicial (similar a @BeforeClass en TestNG)
-    driver = webdriver.Chrome()
+    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+    chrome_options = Options()
+    options = [
+        "--headless",
+        "--disable-gpu",
+        "--window-size=1920,1200",
+        "--ignore-certificate-errors",
+        "--disable-extensions",
+        "--no-sandbox",
+        "--disable-dev-shm-usage"
+    ]
+    for option in options:
+        chrome_options.add_argument(option)
+
+    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
     yield driver
     # Finalización (similar a @AfterClass en TestNG)
     driver.quit()
@@ -12,6 +30,6 @@ def driver():
 def test_example(driver):
     # Prueba simple con Selenium
     driver.get("https://www.google.com")
-    
+
     # Toma de captura de pantalla
     driver.save_screenshot('screenshot.png')
