6971e89dc80e7905af17c13778cb598139d15b09
Vicente García Martínez <vis_97@outlook.com>
2024-10-24 11:57:49 -0600

    Update GoogleScreenshotTest.java

--- src/test/java/com/example/GoogleScreenshotTest.java
+++ src/test/java/com/example/GoogleScreenshotTest.java
@@ -42,11 +42,11 @@ public class GoogleScreenshotTest {
     }
 
     private void generatePDF(String imagePath, String message) throws IOException {
-            PDDocument document = new PDDocument()
+            PDDocument document = new PDDocument();
             PDPage page = new PDPage();
             document.addPage(page);
 
-            PDPageContentStream contentStream = new PDPageContentStream(document, page)
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
             // Añadir el texto al PDF
             contentStream.beginText();
             contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
